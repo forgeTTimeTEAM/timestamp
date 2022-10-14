c03f81a24c20e0bf0c88c79f630b45ee54ae4d83
alvseven <alv7contato@outlook.com>
2022-10-14 15:52:41 -0300

    feat: initial header

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const HeaderContainer = styled.header`
+  display: flex;
+  gap: 2rem;
+  will-change: 10;
+  h1 {
+    font-size: 2rem;
+  }
+`;
+
+export { HeaderContainer };
