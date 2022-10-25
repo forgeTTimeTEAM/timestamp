f77cd8c6dce33314c1f810e4bac17a062cda90ef
alvseven <alv7contato@outlook.com>
2022-10-25 13:23:08 -0300

    feat: dashboard page base

---
+++ pages/dashboard.tsx
@@ -0,0 +1,11 @@
+import SearchResultsContainer from "../components/SearchResultsContainer";
+
+function Dashboard() {
+  return (
+    <>
+      <SearchResultsContainer />
+    </>
+  );
+}
+
+export default Dashboard;
