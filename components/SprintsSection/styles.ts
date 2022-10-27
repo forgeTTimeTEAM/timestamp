42c900a9da6dd1e6b0b4d52c118973b72e75b3ad
alvseven <alv7contato@outlook.com>
2022-10-27 20:36:16 -0300

    fix: solved conflicts

--- components/SprintsSection/styles.ts
+++ components/SprintsSection/styles.ts
@@ -1,51 +1,39 @@
 import styled from "styled-components";
 
 const StyledSection = styled.section`
-<<<<<<< HEAD
+  width: 80%;
+  margin: 1.5rem 0;
+
+  ul {
     display: flex;
-    align-items: center;
+    flex-wrap: wrap;
     justify-content: center;
+    align-items: center;
+    gap: 2.2rem;
 
-    margin: 1.5rem auto;
+    @media screen and (max-width: 1024px) {
+      justify-content: flex-start;
+      flex-wrap: nowrap;
+      overflow-x: auto;
+      padding: 1rem;
 
-    max-width: 1200px;
-    width: 100%;
+      ::-webkit-scrollbar {
+        width: 6px;
+        height: 6px;
+      }
 
-=======
-    width: 80%;
-    margin: 1.5rem 0;
-
->>>>>>> origin/develop
-    ul {
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: center;
-        align-items: center;
-        gap: 2.2rem;
-
-        @media screen and (max-width: 1024px) {
-            justify-content: flex-start;
-            flex-wrap: nowrap;
-            overflow-x: auto;
-            padding: 1rem;
-
-            ::-webkit-scrollbar {
-                width: 6px;
-                height: 6px;
-            }
-
-            ::-webkit-scrollbar-thumb {
-                background: #96a0b9;
-                border-radius: 50px;
-            }
-        }
+      ::-webkit-scrollbar-thumb {
+        background: #96a0b9;
+        border-radius: 50px;
+      }
     }
+  }
 `;
 
 const IconContainer = styled.div`
-    display: flex;
-    justify-content: flex-end;
-    color: #d3e0f0;
+  display: flex;
+  justify-content: flex-end;
+  color: #d3e0f0;
 `;
 
 export { StyledSection, IconContainer };
