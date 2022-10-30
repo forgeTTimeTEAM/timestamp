ec30d133af5e7f6a75b845cdaecbeca849224f11
Giuseppe Dias <giuseppe.dias17@gmail.com>
2022-10-30 20:18:18 -0300

    fix: edit message yup validator

--- components/FormFields/LoginForm/index.tsx
+++ components/FormFields/LoginForm/index.tsx
@@ -4,6 +4,7 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { FormProvider, useForm } from "react-hook-form";
 import { BiLowVision, BiShowAlt } from "react-icons/bi";
 
+import { useRouter } from "next/router";
 import InputField from "../Input";
 import { loginSchema } from "../validators";
 import { StyledForm } from "./styles";
@@ -16,14 +17,16 @@ export interface ILoginFields {
 function LoginForm() {
   const methods = useForm<ILoginFields>({ resolver: yupResolver(loginSchema) });
   const { handleSubmit, setFocus } = methods;
+  const router = useRouter();
+  
   const [seePass, setSeePass] = useState(false);
   const passwordType: "text" | "password" = seePass ? "text" : "password";
-
+  
   const passwordIcon = seePass ? <BiShowAlt /> : <BiLowVision />;
 
   return (
     <FormProvider {...methods}>
-      <StyledForm onSubmit={handleSubmit((data) => console.log(data))}>
+      <StyledForm onSubmit={handleSubmit((data) => router.push("/"))}>
         <div>
           <InputField name="email" label="Email" />
           <InputField name="password" label="Senha" type={passwordType}>
--- components/FormFields/validators/index.ts
+++ components/FormFields/validators/index.ts
@@ -2,8 +2,11 @@ import * as yup from "yup";
 import { ILoginFields } from "../LoginForm";
 
 const loginSchema: yup.SchemaOf<ILoginFields> = yup.object().shape({
-  email: yup.string().required().email(),
-  password: yup.string().required(),
+  email: yup
+    .string()
+    .required("Preencha o campo email")
+    .email("Formato de email inválido"),
+  password: yup.string().required("Preencha o campo senha"),
 });
 
 export { loginSchema };
