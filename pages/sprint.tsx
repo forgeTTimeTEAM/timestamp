9892bdbe609c7432714c2f9303aaed591a907e70
joaoparaujocr <araujo.joao.paulo.jaa@gmail.com>
2022-10-31 14:44:46 -0400

    fix: centralize search container

--- pages/sprint.tsx
+++ pages/sprint.tsx
@@ -1,16 +1,17 @@
 import Header from "../components/Header";
 import SearchResultsContainer from "../components/SearchResultsContainer";
 import SprintContainer from "../components/SprintContainer";
+
 import { MainContainer } from "../styles/dashboard/styles";
 
 function Sprint() {
   return (
     <>
       <Header dashboard />
-      <>
+      <MainContainer>
         <SprintContainer />
         <SearchResultsContainer />
-      </>
+      </MainContainer>
     </>
   );
 }
